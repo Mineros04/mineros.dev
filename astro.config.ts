86ce6c94ee8d7fba1cc7f4520fc20e95d884b682
Mineros04 <adam.jaros04@gmail.com>
2026-05-16 01:44:45 +0200

    feat: add umami analytics

--- astro.config.ts
+++ astro.config.ts
@@ -1,16 +1,24 @@
-// @ts-check
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import vue from "@astrojs/vue";
 import tailwindcss from "@tailwindcss/vite";
+import createUmami from "@yeskunall/astro-umami";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
 
+// Analytics
+const umami = createUmami({
+  id: process.env.UMAMI_WEBSITE_ID ?? "",
+  endpointUrl: process.env.UMAMI_HOST ?? ""
+});
+
 // https://astro.build/config
 export default defineConfig({
+  site: process.env.BASE_DOMAIN ?? "",
+  trailingSlash: "never",
+
   vite: {
     plugins: [tailwindcss()]
   },
-
-  integrations: [mdx(), icon(), sitemap(), vue()]
+  integrations: [mdx(), icon(), sitemap(), vue(), umami]
 });
