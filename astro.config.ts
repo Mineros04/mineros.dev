8bd05ee267f8100af1da4a36c1b5fa11f41444c8
Mineros04 <adam.jaros04@gmail.com>
2026-05-16 19:42:07 +0200

    fix(config): revert to native .env loading

--- astro.config.ts
+++ astro.config.ts
@@ -5,23 +5,26 @@ import tailwindcss from "@tailwindcss/vite";
 import createUmami from "@yeskunall/astro-umami";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
-import { loadEnv } from "vite";
 
-const env = loadEnv(process.env.NODE_ENV || "production", process.cwd(), "");
+try {
+  process.loadEnvFile();
+} catch {
+  console.warn("Failed to load .env file.");
+}
 
 // Analytics
 const umami = createUmami({
-  id: env.UMAMI_WEBSITE_ID ?? "",
-  endpointUrl: env.UMAMI_HOST ?? ""
+  id: process.env.UMAMI_WEBSITE_ID ?? "",
+  endpointUrl: process.env.UMAMI_HOST ?? ""
 });
 
 // https://astro.build/config
 export default defineConfig({
-  site: env.BASE_DOMAIN ?? "http://localhost:4321",
+  site: process.env.BASE_DOMAIN ?? "http://localhost:4321",
   trailingSlash: "never",
 
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss() as any]
   },
   integrations: [mdx(), icon(), sitemap(), vue(), umami]
 });
