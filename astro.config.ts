0c19af74c1413325f9e41c7257484efd466a5b8e
Mineros04 <adam.jaros04@gmail.com>
2026-05-27 18:28:58 +0200

    feat(config): migrate vue to svelte
    Since this site acts as a simple personal portfolio / blog, there is no need for a heavy framework like Vue. Even though I like Vue, Svelte fits the purpose of this project better, enabling smaller bundle sizes.
    
    Vue components and libraries not supported anymore. All interactivity must be made in Svelte 5 components instead.

--- astro.config.ts
+++ astro.config.ts
@@ -1,10 +1,11 @@
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import vue from "@astrojs/vue";
 import tailwindcss from "@tailwindcss/vite";
 import createUmami from "@yeskunall/astro-umami";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
+
+import svelte from "@astrojs/svelte";
 
 try {
   process.loadEnvFile();
@@ -26,5 +27,5 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss() as any]
   },
-  integrations: [mdx(), icon(), sitemap(), vue(), umami]
+  integrations: [mdx(), icon(), sitemap(), umami, svelte()]
 });
